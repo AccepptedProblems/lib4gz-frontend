c33ea2a0a2a70ed2490c8dfdda84aca11c2cc278
ThanhNam <thanhnam.nguyen@lingbook.io>
2026-03-13 19:38:28 +0700

    deploy: 2026-03-13 19:38:28

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,27 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  readonly userInitial = this.authService.getUserName().charAt(0).toUpperCase() || 'U';
+  readonly userName = this.authService.getUserName();
+  readonly avatarMenuOpen = signal(false);
+
+  toggleAvatarMenu(event: Event): void {
+    event.stopPropagation();
+    this.avatarMenuOpen.update(open => !open);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  @HostListener('document:click')
+  closeAvatarMenu(): void {
+    if (this.avatarMenuOpen()) {
+      this.avatarMenuOpen.set(false);
+    }
+  }
 }
